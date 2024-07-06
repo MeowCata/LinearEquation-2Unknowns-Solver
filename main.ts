d0047e06a8b5886956732e518e9efa1ebf5638a7
DingDang <97514372+MeowCata@users.noreply.github.com>
2024-07-06 09:59:16 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+function inputParameter () {
+    basic.showString("a")
+    basic.showNumber(a)
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            a += -1
+            basic.showNumber(a)
+        } else if (input.buttonIsPressed(Button.B)) {
+            a += 1
+            basic.showNumber(a)
+        }
+        basic.pause(50)
+    }
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showNumber(a)
+    basic.showString("b")
+    basic.showNumber(b)
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            b += -1
+            basic.showNumber(b)
+        } else if (input.buttonIsPressed(Button.B)) {
+            b += 1
+            basic.showNumber(b)
+        }
+        basic.pause(50)
+    }
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showNumber(b)
+    basic.showString("c")
+    basic.showNumber(c)
+    while (!(input.buttonIsPressed(Button.AB))) {
+        if (input.buttonIsPressed(Button.A)) {
+            c += -1
+            basic.showNumber(c)
+        } else if (input.buttonIsPressed(Button.B)) {
+            c += 1
+            basic.showNumber(c)
+        }
+        basic.pause(50)
+    }
+    basic.clearScreen()
+    basic.pause(200)
+    basic.showNumber(c)
+    basic.clearScreen()
+}
+function solve () {
+    basic.showString("a=" + a + " b=" + b + " c=" + c)
+    delta = b * b - 4 * (a * c)
+    basic.showString("d=" + delta)
+    if (delta < 0) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.pause(500)
+        basic.clearScreen()
+    } else {
+        basic.showString("x1=")
+    }
+}
+input.onPinPressed(TouchPin.P1, function () {
+    inputParameter()
+})
+let delta = 0
+let b = 0
+let c = 0
+let a = 0
+a = 0
+c = 0
+b = 0
+delta = 0
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
