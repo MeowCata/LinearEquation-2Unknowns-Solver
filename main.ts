66272fab36e974c325d154fcf56935c20f58f1b4
DingDang <97514372+MeowCata@users.noreply.github.com>
2024-07-06 10:34:08 +0800

    supported fraction and a few updates

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function inputParameter () {
     basic.showString("a")
     basic.showNumber(a)
-    while (!(input.buttonIsPressed(Button.AB))) {
+    while (!(input.pinIsPressed(TouchPin.P2))) {
         if (input.buttonIsPressed(Button.A)) {
             a += -1
             basic.showNumber(a)
@@ -15,7 +15,7 @@ function inputParameter () {
     basic.showNumber(a)
     basic.showString("b")
     basic.showNumber(b)
-    while (!(input.buttonIsPressed(Button.AB))) {
+    while (!(input.pinIsPressed(TouchPin.P2))) {
         if (input.buttonIsPressed(Button.A)) {
             b += -1
             basic.showNumber(b)
@@ -29,7 +29,7 @@ function inputParameter () {
     basic.showNumber(b)
     basic.showString("c")
     basic.showNumber(c)
-    while (!(input.buttonIsPressed(Button.AB))) {
+    while (!(input.pinIsPressed(TouchPin.P2))) {
         if (input.buttonIsPressed(Button.A)) {
             c += -1
             basic.showNumber(c)
@@ -54,17 +54,44 @@ function solve () {
             . # # # .
             # . . . #
             `)
-        basic.pause(500)
+        basic.pause(1000)
+        basic.clearScreen()
+    } else if (Math.sqrt(delta) % 1 != 0) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            . . . # .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(1000)
         basic.clearScreen()
     } else {
-        basic.showString("x1=" + (0 - b + Math.sqrt(delta)) / (2 * a))
-        basic.showString("x2=" + (0 - b - Math.sqrt(delta)) / (2 * a))
+        x1 = (0 - b + Math.sqrt(delta)) / (2 * a)
+        x2 = (0 - b - Math.sqrt(delta)) / (2 * a)
+        if (x1 % 1 == 0 && x2 % 1 == 0) {
+            basic.showString("x1=" + x1)
+            basic.showString("x2=" + x2)
+        } else {
+            if (x1 % 1 == 0) {
+                basic.showString("x1=" + x1)
+            } else {
+                basic.showString("x1=" + (0 - b + Math.sqrt(delta)) + "/" + 2 * a)
+            }
+            if (x2 % 1 == 0) {
+                basic.showString("x2=" + x2)
+            } else {
+                basic.showString("x2=" + (0 - b - Math.sqrt(delta)) + "/" + 2 * a)
+            }
+        }
     }
 }
 input.onPinPressed(TouchPin.P1, function () {
     inputParameter()
     solve()
 })
+let x2 = 0
+let x1 = 0
 let delta = 0
 let b = 0
 let c = 0
@@ -73,4 +100,7 @@ a = 1
 c = 0
 b = 0
 delta = 0
+x1 = 0
+x2 = 0
 pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
